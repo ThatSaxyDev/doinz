056c1c2bb507f88aff7bbba26eea67d38a800a2b
Kiishi David <user@Kiishis-MacBook-Pro.local>
2023-06-29 07:14:11 +0100

    was working on a pizza concept

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:doinz/counter/views/counter_view.dart';
+import 'package:doinz/pizza/views/pizza_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -30,56 +31,8 @@ class Doinz extends StatelessWidget {
               brightness: Brightness.dark,
               useMaterial3: true,
             ),
-            home: const CounterView(),
+            home: const PizzaView(),
           );
         });
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
   }
 }
